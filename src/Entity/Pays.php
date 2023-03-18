72f94aab495fe20097d6b386de36b5fdd5bb0f36
Slloth <fiquet.noah@gmail.com>
2023-03-18 19:02:06 +0100

    relation ManyToOne

--- src/Entity/Pays.php
+++ src/Entity/Pays.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PaysRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PaysRepository::class)]
@@ -19,6 +21,18 @@ class Pays
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
+    #[ORM\OneToMany(mappedBy: 'pays', targetEntity: AdresseUtil::class, orphanRemoval: true)]
+    private Collection $adressesUtil;
+
+    #[ORM\OneToMany(mappedBy: 'pays', targetEntity: InfoPratique::class, orphanRemoval: true)]
+    private Collection $infosPratique;
+
+    public function __construct()
+    {
+        $this->adressesUtil = new ArrayCollection();
+        $this->infosPratique = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -32,6 +46,66 @@ class Pays
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, AdresseUtil>
+     */
+    public function getAdressesUtil(): Collection
+    {
+        return $this->adressesUtil;
+    }
+
+    public function addAdressesUtil(AdresseUtil $adressesUtil): self
+    {
+        if (!$this->adressesUtil->contains($adressesUtil)) {
+            $this->adressesUtil->add($adressesUtil);
+            $adressesUtil->setPays($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAdressesUtil(AdresseUtil $adressesUtil): self
+    {
+        if ($this->adressesUtil->removeElement($adressesUtil)) {
+            // set the owning side to null (unless already changed)
+            if ($adressesUtil->getPays() === $this) {
+                $adressesUtil->setPays(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, InfoPratique>
+     */
+    public function getInfosPratique(): Collection
+    {
+        return $this->infosPratique;
+    }
+
+    public function addInfosPratique(InfoPratique $infosPratique): self
+    {
+        if (!$this->infosPratique->contains($infosPratique)) {
+            $this->infosPratique->add($infosPratique);
+            $infosPratique->setPays($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInfosPratique(InfoPratique $infosPratique): self
+    {
+        if ($this->infosPratique->removeElement($infosPratique)) {
+            // set the owning side to null (unless already changed)
+            if ($infosPratique->getPays() === $this) {
+                $infosPratique->setPays(null);
+            }
+        }
 
         return $this;
     }
